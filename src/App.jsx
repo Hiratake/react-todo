9bf39040e083d73131c6770bc148c42e59699e3d
Hiratake <hiratake@hiratake.xyz>
2021-09-01 23:10:47 +0900

    chore: イテレーションによるレンダリング

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import Todo from './components/Todo'
 
 const App = (props) => {
+  const taskList = props.tasks.map((task) => (
+    <Todo
+      key={task.id}
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+    />
+  ))
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -48,12 +57,14 @@ const App = (props) => {
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
-        <Todo name="Eat" completed={true} id="todo-0" />
-        <Todo name="Sleep" completed={false} id="todo-1" />
-        <Todo name="Repeat" completed={false} id="todo-2" />
+        {taskList}
       </ul>
     </div>
   )
+}
+
+App.propTypes = {
+  tasks: PropTypes.array,
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import './index.css'
 
+const Data = [
+  { id: 'todo-0', name: 'Eat', completed: true },
+  { id: 'todo-1', name: 'Sleep', completed: false },
+  { id: 'todo-2', name: 'Repeat', completed: false },
+]
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App tasks={Data} />
   </React.StrictMode>,
   document.getElementById('root'),
 )
