56aed25ea2f3413efa4892c6b65ccbb143cefd59
Hiratake <hiratake@hiratake.xyz>
2021-09-01 11:13:55 +0900

    fix: testのファイルでlintのエラーが発生していたのを修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,22 +2,27 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
+    'jest/globals': true,
   },
   extends: [
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
-    'standard'
+    'standard',
   ],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: [
-    'react'
+    'react',
+    'jest',
   ],
-  rules: {}
+  rules: {
+    'comma-dangle': ['error', 'always-multiline'],
+    'brace-style': ['error', 'stroustrup'],
+  },
 }
