1bf11d93f4d665a254774a2dd18b94c626de58c9
Hiratake <hiratake@hiratake.xyz>
2021-09-02 08:37:23 +0900

    feat: タスクのフィルタリング機能を追加

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,25 @@ import FilterButton from './components/FilterButton'
 import Form from './components/Form'
 import Todo from './components/Todo'
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed,
+}
+const FILTER_NAMES = Object.keys(FILTER_MAP)
+
 const App = (props) => {
   const [tasks, setTasks] = useState(props.tasks)
+  const [filter, setFilter] = useState('All')
+
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      onSet={setFilter}
+    />
+  ))
   const toggleTaskCompleted = (id) => {
     const updatedTasks = tasks.map((task) => {
       if (id === task.id) {
@@ -30,17 +47,19 @@ const App = (props) => {
     const remainingTasks = tasks.filter(task => id !== task.id)
     setTasks(remainingTasks)
   }
-  const taskList = tasks.map((task) => (
-    <Todo
-      key={task.id}
-      id={task.id}
-      name={task.name}
-      completed={task.completed}
-      onCompleted={toggleTaskCompleted}
-      onEdit={editTask}
-      onDelete={deleteTask}
-    />
-  ))
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
+      <Todo
+        key={task.id}
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        onCompleted={toggleTaskCompleted}
+        onEdit={editTask}
+        onDelete={deleteTask}
+      />
+    ))
   const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task'
   const headingText = `${taskList.length} ${tasksNoun} remaining`
   const addTask = (name) => {
@@ -53,9 +72,7 @@ const App = (props) => {
       <h1>TodoMatic</h1>
       <Form onSubmit={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
--- src/components/FilterButton.jsx
+++ src/components/FilterButton.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const FilterButton = (props) => {
   return (
-    <button type="button" className="btn toggle-btn" aria-pressed="true">
+    <button
+      type="button"
+      className="btn toggle-btn"
+      aria-pressed={props.isPressed}
+      onClick={() => props.onSet(props.name)}
+    >
       <span className="visually-hidden">Show </span>
-      <span>all</span>
+      <span>{props.name}</span>
       <span className="visually-hidden"> tasks</span>
     </button>
   )
+}
+
+FilterButton.propTypes = {
+  name: PropTypes.string,
+  isPressed: PropTypes.bool,
+  onSet: PropTypes.func,
 }
 
 export default FilterButton
